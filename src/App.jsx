2e19c0338776eb02a307ae983e008d8ab42cd8de
roger rodriguez <rogerdavid.rd@gmail.com>
2022-12-26 19:41:19 -0300

    modificar carpeta componetes a componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import  Header from "./componentes/Header";
-import  Card from "./componentes/Card";
-import  Footer from "./componentes/Footer";
+import  Header from "./components/Header";
+import  Card from "./components/Card";
+import  Footer from "./components/Footer";
 
 
 const App = ()=> {
